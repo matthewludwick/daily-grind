c72eb4ef923171641bd869b05fed27ac18e37835
Matthew <70419638+matthewludwick@users.noreply.github.com>
2026-05-06 16:42:15 -0700

    fist version of coffee object

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,3 +1,21 @@
+/*
+
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+
+
+    pic - image src
+    alt - the alt tag for the image
+    desc - a description of the coffee
+    day - the day of the week for the coffee
+    color - color associated with coffee
+    name - the name of the coffee
+
+*/
+
+
+
 let myDate = new Date();
 let myDay = myDate.getDay();
 let today = "";
@@ -10,6 +28,16 @@ switch(myDay){
 
  	case 2:
    		today = "Tuesday";
+        coffee = {
+            name:"Bubble Tea",
+            pic:"bubble-tea.jpg",
+            alt:"A pic of a yummy bubble tea.",
+            color:"pink",
+            day:"Tuesday",
+            desc:`I like me some bubble tea!`
+
+        };
+
  	break;
 
  	default:
@@ -18,4 +46,5 @@ switch(myDay){
 }
 
 
+console.log(coffee);
 alert(today);
